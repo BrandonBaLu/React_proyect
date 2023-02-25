0b53bb257e0e18003ef810001afc5046b145b36a
BrandonBaLu <1719110177@utectulancingo.edu.mx>
2023-02-25 03:02:19 +0000

    React

--- App.js
+++ App.js
@@ -1,62 +1,73 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React, {Component} from "react";
+import {StyleSheet, TouchableOpacity ,Text, View} from 'react-native';
 
-import AppNavigator from './navigation/AppNavigator';
+/*const HelloWorkApp = ()=> {
+  return (
+    <View
+    style ={{
+      flex:1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text>Hello World!</Text>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+export default HelloWorkApp;
+*/
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
-  }
+class App extends Component{
+  //incrementar y decrementar un contador de 1 en 1
+    constructor(props){
+      super(props);
+      this.state = {
+        contador: 0
+      }
+    }
+    incrementar = () => {
+        this.setState({
+            contador: this.state.contador + 1
+        });
+        }
+    decrementar = () => {
+        this.setState({
+            contador: this.state.contador - 1
+        });
+        }
+    render(){
+        return (
+            <View style={styles.container}>
+            <Text>Contador: {this.state.contador}</Text>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={this.incrementar}>
+                <Text>Incrementar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={this.decrementar}>
+                <Text>Decrementar</Text>
+            </TouchableOpacity>
+            </View>
+        );
+    }    
+  
 }
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
 
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  button:{
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    margin: 10,
+  }
 });
+export default App;
